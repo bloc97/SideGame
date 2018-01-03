eaf6b8620593a8b6783c06af205cc704764e2411
dragonpan2 <panbeining@hotmail.com>
2018-01-03 13:27:59 -0500

    007

---
+++ SideGame/src/sidegame/CrewMember.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package sidegame;
+
+/**
+ *
+ * @author panbe
+ */
+public class CrewMember {
+    String name;
+    String occupation;
+    int dayWithoutFood;
+    int dayWithoutWater;
+    int moral; //min 0 max 100        
+    int age;
+    int goldContributionDaily;
+    int goldContributionBefore;
+    int goldContributionAfter;
+    
+    public CrewMember() {
+    }
+    
+    
+}
