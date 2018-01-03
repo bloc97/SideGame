efb641882746aa7a50d7fc83a747b04bd416216c
bloc97 <bloc97@users.noreply.github.com>
2018-01-03 13:27:03 -0500

    009 fixed newline indentation

--- SideGame/src/sidegame/CargoTest.java
+++ SideGame/src/sidegame/CargoTest.java
@@ -20,27 +20,27 @@ public class CargoTest {
         
         System.out.println("Bought 5 coal");
         inventory.addCargo(new CaravanCargo(coalID, "Coal", 5));
-        System.out.println(inventory);
+        System.out.println(inventory + "\n");
         
         System.out.println("Bought 1 wood");
         inventory.addCargo(new CaravanCargo(woodID, "Wood", 1));
-        System.out.println(inventory);
+        System.out.println(inventory + "\n");
         
         System.out.println("Tried to sell 6 coal");
         inventory.removeCargo(new CaravanCargo(coalID, "Coal", 6));
-        System.out.println(inventory);
+        System.out.println(inventory + "\n");
         
         System.out.println("Sold 4 coal");
         inventory.removeCargo(new CaravanCargo(coalID, "Coal", 4));
-        System.out.println(inventory);
+        System.out.println(inventory + "\n");
         
         System.out.println("Tried to buy 5 beer");
         inventory.addCargo(new CaravanCargo(beerID, "Beer", 5));
-        System.out.println(inventory);
+        System.out.println(inventory + "\n");
         
         System.out.println("Bought 4 beer");
         inventory.addCargo(new CaravanCargo(beerID, "Beer", 4));
-        System.out.println(inventory);
+        System.out.println(inventory + "\n");
         
         
         
